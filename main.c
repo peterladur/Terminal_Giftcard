ce475616e4662113f3a2b35cff2bb719b0f6d56a
Peter Ladur <peter.ladour@gmail.com>
2026-03-29 14:26:04 +1300

    started work on boundary function

--- main.c
+++ main.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
+
+int boundary()
+{
+    //Create a boundary array
+    int n = 10;
+    int boundary_values[2][n];
+
+    //Open the ascii file
+    FILE *ascii_file;
+    ascii_file = fopen("ASCII_art.txt", "r");
+
+    while 
+
+    return 0;
+}
+
+
+
 int main(){
+
     
+
 }
