ff02642890402188f094c7498f6b34e55d8b5bbe
Peter Ladur <peter.ladour@gmail.com>
2026-03-30 15:22:06 +1300

    Updated the structure/plan of the project

--- main.c
+++ main.c
@@ -1,15 +1,65 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 #define TRUE 1
 #define FALSE 0
 
-int boundary()
+
+//CONSTANTS
+int WIDTH = 100;
+int HEIGHT = 40;
+int ART_RIGHT_BOUNDARY = 50;
+
+//Structs
+struct Position
 {
-    //Create a boundary array
-    int n = 8;
-    int line_len = 100;
-    int boundary_values[2][n];
+    int x;
+    int y;
+};
+
+//Forward Declaration
+void calculate_x_z_position(int x, int linewidth, int angluar_velocity, float t);
+void boundary(int n, int line_len, int boundary_values[2][n]);
+void display_buffer(int n, int line_length, char buffer[n][line_length]);
+void calculate_x_z_position(int x, int linewidth, int angluar_velocity, float t);
+void init_buffer(int n, int line_len, char buffer[n][line_len]);
+
+
+void init_buffer(int n, int line_len, char buffer[n][line_len])
+{
+    //Open the File
+    FILE *ascii_art_file;
+    ascii_art_file = fopen("ASCII_art.txt", "r");
+
+    //Reading the file
+    char file_contents[line_len];
+
+    if (ascii_art_file == NULL)
+    {
+        printf("Not able to open the file");
+    }
+    else
+    {
+        int line = 0;
+        while (fgets(file_contents, line_len, ascii_art_file))
+        {   
+            for (int i = 0; i < strlen(file_contents); i++)
+            {    
+                buffer[line][i] = file_contents[i];
+            }
+            line++;
+        }
+    }
+
+    //Close the file
+    fclose(ascii_art_file);
+
+
+}
+
+void boundary(int n, int line_len, int boundary_values[2][n])
+{
 
     //Open the ascii file
     FILE *ascii_file;
@@ -35,13 +85,13 @@ int boundary()
             int i = 0;
             while (i < strlen(file_contents) && (found_end == FALSE))
             {   
-                printf("%c", file_contents[i]);
-
+                //Looks for the initial # character
                 if (file_contents[i] == '#' && (found_start == FALSE)) 
                 {
                     found_start = TRUE;
                     boundary_values[0][line] = i;
                 }
+                //Looks for the final # character
                 if ((file_contents[i] == ' '  && (found_start == TRUE)) ) 
                 {
                     found_end = TRUE;
@@ -49,31 +99,76 @@ int boundary()
                 }
                 i++;
             }
-            //printf("\n");
 
 
             line++;
         }
 
     }
-    printf("\n");
+
+    //Close the file
     fclose(ascii_file);
-
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%d ", boundary_values[i][j]);
-        }
-        printf("\n");  // new line after each row
-    }
-
-    return 0;
 }
 
+void display_buffer(int n, int line_length, char buffer[n][line_length])
+{
 
+    for (int y = 0; y < n; y++)
+    {
+        int x = 0;
+        while ((x < line_length) && (buffer[y][x] != 0))
+        {
+
+            printf("%c", buffer[y][x]);
+            x++;
+        }
+    }
+    printf("\n");
+
+}
+
+/*void animation(int n, int line_length, int boundary_values[2][n]){
+
+
+    
+    display_buffer(n, line_length, buffer);
+
+    //calculate_x_z_position(1, line_length, 1, 0.2);
+    
+}*/
+
+void calculate_x_z_position(int x, int linewidth, int angluar_velocity, float t)
+{   
+    int r = (x - linewidth/2);
+    printf("%d\n", r);
+    printf("%f\n", t);
+    float x_new = r * cos(angluar_velocity * t);
+    float z_new = r * sin(angluar_velocity * t);
+}
 
 int main(){
 
-    boundary();
+    //Initialise Variables
+    int line_len = 100;
+    int boundary_values[2][n];
+    char buffer[n][line_len];
+
+    //Initialise Buffer
+    init_buffer(n, line_len, buffer);
+
+    //Initialise Text
+    //init_text();
+
+    //Display Buffer & Text
+    display_buffer(n, line_len, buffer);
+
+    //OPTIONAL: Animation -> Rotates the 2d image in 3d
+
+    //Set the boundary Array
+    //boundary(n, line_len, boundary_values);
+
+    //Displays the characters between the boundary
+    //animation(n, line_len, boundary_values);
 
     return 0;
 
--- plan.md
+++ plan.md
@@ -1,10 +1,12 @@
 **Plan**
 
 
-1. The ASCII art should be in # in a .txt file.
-2. The file is read line by line and added to an array of arrays of fixed length
-3. We grab the boundary of the image and then rotate that boundary in z coordinates.
-4. Then go through the char line by line and see if it should be inside the boundary
-    If a char is meant to be inside, we can display it
-    If a char is meant to be outside we can ommit it
-    We can modify the chars based on the z value (for example changing their colours)
+The ASCII art is stored in a .txt file
+The text is stored in a text.txt file
+
+A buffer with height and width is generated
+
+The art is processesed and displayed on the left
+The text is processed and displayed on the right
+
+The boundary for art piece is then captured and everything within it, is rotated/animated and the whole buffer is displayed.
